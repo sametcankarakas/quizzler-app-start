75bdbb8479cad162c521d414f7de10099e2313b9
Samet Can KARAKAŞ <scankarakas@hotmail.com>
2021-11-21 23:44:22 +0300

    working on it, canvas created. its time to add questions where taken with api.

--- ui.py
+++ ui.py
@@ -1,2 +1,24 @@
+from tkinter import *
+from data import *
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.canvas.grid(column=0, row=1, columnspan=2, padx=50, pady=50)
+        self.language_text = self.canvas.create_text(150, 125, text="language", fill="black",
+                                                     font=("Arial", 20, "italic"))
+        self.score = Label(text=f"Score: ", bg=THEME_COLOR, fg="white")
+        self.score.grid(column=1, row=0)
+        self.true_button_image = PhotoImage(file="./images/true.png")
+        self.true_button = Button(image=self.true_button_image, command=None, highlightthickness=0, bd=0)
+        self.true_button.grid(column=0, row=2)
+        self.false_button_image = PhotoImage(file="./images/false.png")
+        self.false_button = Button(image=self.false_button_image, command=None, highlightthickness=0, bd=0)
+        self.false_button.grid(column=1, row=2)
+        self.window.mainloop()
